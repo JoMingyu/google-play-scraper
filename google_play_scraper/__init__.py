1d9a9ca31b530e1f40fdf479e7e08547ed3595ab
Klaus Luhan <klaus.luhan@gmail.com>
2021-02-18 19:13:28 +0100

    add export for permissions-endpoint

--- google_play_scraper/__init__.py
+++ google_play_scraper/__init__.py
@@ -1,5 +1,6 @@
 from .constants.google_play import Sort
 from .features.app import app
 from .features.reviews import reviews, reviews_all
+from .features.permissions import permissions
 
-VERSION = __version__ = "0.1.2"
+VERSION = __version__ = "0.1.3"
