937566d649cc537d6f6b5b20e3d1ac75f653804c
Eitol <hector.oliveros.leon@gmail.com>
2024-05-11 15:27:06 -0400

    Implement retry logic for rate limit exceeded error
    In the utility "request" of the google play scraper, we have implemented a retry logic whenever a 'com.google.play.gateway.proto.PlayGatewayError' (rate limit exceeded error) is encountered. The function will now retry up to a maximum of 3 times with an increasing delay time between each retry in order to respect the server's rate limit.

--- google_play_scraper/utils/request.py
+++ google_play_scraper/utils/request.py
@@ -6,6 +6,9 @@ from google_play_scraper.exceptions import ExtraHTTPError, NotFoundError
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
+MAX_RETRIES = 3
+RATE_LIMIT_DELAY = 5
+
 def _urlopen(obj):
     try:
         resp = urlopen(obj)
@@ -21,7 +24,21 @@ def _urlopen(obj):
 
 
 def post(url: str, data: Union[str, bytes], headers: dict) -> str:
-    return _urlopen(Request(url, data=data, headers=headers))
+    last_exception = None
+    rate_exceeded_count = 0
+    for _ in range(MAX_RETRIES):
+        try:
+            resp = _urlopen(Request(url, data=data, headers=headers))
+        except Exception as e:
+            last_exception = e
+            continue
+        if 'com.google.play.gateway.proto.PlayGatewayError' in resp:
+            rate_exceeded_count += 1
+            last_exception = Exception('com.google.play.gateway.proto.PlayGatewayError')
+            time.sleep(RATE_LIMIT_DELAY*rate_exceeded_count)
+            continue
+        return resp
+    raise last_exception
 
 
 def get(url: str) -> str:
