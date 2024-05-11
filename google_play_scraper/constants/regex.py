bb9e90dc31b04ce3c2723bd10eb80f186c9dcbee
Eitol <hector.oliveros.leon@gmail.com>
2024-05-11 15:25:44 -0400

    Update regex patterns in scraper constants
    Raw strings are now used to define the patterns which increases readability and avoids the backslash escaping issue.

--- google_play_scraper/constants/regex.py
+++ google_play_scraper/constants/regex.py
@@ -2,9 +2,9 @@ import re
 
 
 class Regex:
-    NOT_NUMBER = re.compile("[^\d]")
-    SCRIPT = re.compile("AF_initDataCallback[\s\S]*?<\/script")
+    NOT_NUMBER = re.compile(r"\D")
+    SCRIPT = re.compile(r"AF_initDataCallback[\s\S]*?</script")
     KEY = re.compile("(ds:.*?)'")
-    VALUE = re.compile("data:([\s\S]*?), sideChannel: {}}\);<\/")
-    REVIEWS = re.compile("\)]}'\n\n([\s\S]+)")
-    PERMISSIONS = re.compile("\)]}'\n\n([\s\S]+)")
+    VALUE = re.compile(r"data:([\s\S]*?), sideChannel: {}}\);<\/")
+    REVIEWS = re.compile(r"\)]}'\n\n([\s\S]+)")
+    PERMISSIONS = re.compile(r"\)]}'\n\n([\s\S]+)")
